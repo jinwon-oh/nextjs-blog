4982bb3ca1d4b4423c7fc5d8eddaea26f17d786e
saiyuki9848 <saiyuki9848@gmail.com>
2022-04-30 10:05:15 +0900

    Working upload
    exmple url:
      https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I'm Jinwon Oh.</p>
+        <p><a href="/posts/first-post">First Post</a></p>
+        <p>
+          (This is a sample website - you'll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
+        </p>
+      </section>
+    </Layout>
+  );
+}
