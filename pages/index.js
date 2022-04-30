296b4eab90db74e711e852f90957520c0148ec78
saiyuki9848 <saiyuki9848@gmail.com>
2022-04-30 16:06:39 +0900

    Working upload
    url: https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -42,9 +42,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>I'm Jinwon Oh.</p>
         <p>
-          <a href="/posts/first-post">First Post</a>
-        </p>
-        <p>
           (This is a sample website - you'll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
         </p>
